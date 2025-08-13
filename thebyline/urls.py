0fcb5cfae84050fc2fb35b0e0e9379b624206c90
nitish bahuguna <webwork150@gmail.com>
2025-08-13 14:22:05 +0530

    seo/google page

--- thebyline/urls.py
+++ thebyline/urls.py
@@ -19,7 +19,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
+from django.templatetags.static import static
 from .sitemaps import ArticleSitemap
 
 sitemaps = {
@@ -31,6 +32,8 @@ urlpatterns = [
     path('', include('blog.urls')),  # ← include your blog URLs here
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    # Ensure Google and browsers can fetch favicon at the root
+    path('favicon.ico', RedirectView.as_view(url=static('blog/images/Logo.ico'), permanent=True)),
 ]
 
 if settings.DEBUG:
