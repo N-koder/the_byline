391ba9e7de92f6346294cc87005d63e8c2c2dbf2
Nitish Bahuguna <68819413+N-koder@users.noreply.github.com>
2025-08-15 16:10:15 +0530

    Update urls.py
    individual author articles

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('category/<str:category_name>/', views.category_articles, name='category_articles'),
     path('article/<slug:slug>/', views.article_detail, name='article_detail'),
     path('tag/<slug:slug>/', views.tagged_articles, name="tagged_articles"),
+    path('author/<str:username>/', views.author_articles, name="author_articles"),
     path('contact/', views.contact, name='contact'),
     path('subscribe/', views.subscribe_newsletter, name='subscribe_newsletter'),
     path('about/', views.about, name='about'),
