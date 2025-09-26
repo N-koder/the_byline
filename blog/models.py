ef5855dcae5591d66824c55cdbe6fd31086893a1
Nitish Bahuguna <68819413+N-koder@users.noreply.github.com>
2025-09-26 22:36:33 +0530

    Update models.py
    minor change for sitemap

--- blog/models.py
+++ blog/models.py
@@ -103,3 +103,6 @@ class Podcast(models.Model):
 
     def __str__(self):
         return self.title
+        
+    def get_absolute_url(self):
+        return reverse("podcast_detail", kwargs={"slug": self.slug})
